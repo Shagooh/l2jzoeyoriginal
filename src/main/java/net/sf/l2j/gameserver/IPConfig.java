af63e1af726463d7dcc3e1e259af4f220bb28a34
Asier Paz <asier@championgames.com>
2022-05-07 18:29:14 +0200

    Add removed indentations in blank lines back

--- src/main/java/net/sf/l2j/gameserver/IPConfig.java
+++ src/main/java/net/sf/l2j/gameserver/IPConfig.java
@@ -1,18 +1,18 @@
 /*
  * Copyright © 2004-2022 L2J Server
- *
+ * 
  * This file is part of L2J Server.
- *
+ * 
  * L2J Server is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * L2J Server is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -48,17 +48,17 @@ import java.util.stream.IntStream;
 public class IPConfig implements IXmlReader {
 
 	private static final Logger LOG = Logger.getLogger(IPConfig.class.getName());
-
+	
 	private static final String IP_CONFIG_FILE = "./config/ipconfig.xml";
-
+	
 	private final List<String> _subnets = new ArrayList<>(5);
-
+	
 	private final List<String> _hosts = new ArrayList<>(5);
-
+	
 	public IPConfig() {
 		load();
 	}
-
+	
 	@Override
 	public void load() {
 		final File f = new File(IP_CONFIG_FILE);
